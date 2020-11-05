deae6d8232bfc3f8fbd3d12779e8fa25149dd160
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-05 14:11:13 +0000

    PA1-ans/

---
+++ PA1-ans/nemu/src/cpu/exec/all-instr.h
@@ -0,0 +1,38 @@
+#include "prefix/prefix.h"
+
+#include "data-mov/mov.h"
+#include "data-mov/xchg.h"
+#include "data-mov/movext.h"
+#include "data-mov/cltd.h"
+#include "data-mov/leave.h"
+#include "data-mov/push.h"
+#include "data-mov/pop.h"
+
+#include "arith/dec.h"
+#include "arith/inc.h"
+#include "arith/neg.h"
+#include "arith/imul.h"
+#include "arith/mul.h"
+#include "arith/idiv.h"
+#include "arith/div.h"
+#include "arith/cmp.h"
+
+#include "logic/and.h"
+#include "logic/or.h"
+#include "logic/not.h"
+#include "logic/xor.h"
+#include "logic/sar.h"
+#include "logic/shl.h"
+#include "logic/shr.h"
+#include "logic/shrd.h"
+#include "logic/test.h"
+
+#include "string/rep.h"
+
+#include "misc/misc.h"
+
+#include "special/special.h"
+
+#include "trans/call.h"
+#include "trans/je.h"
+#include "trans/ret.h"
